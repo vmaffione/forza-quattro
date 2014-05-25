9f004e1f9142872f03ccc483a3872926cb55101e
Vincenzo Maffione <v.maffione@gmail.com>
2014-05-25 15:51:13 +0200

    signup: reorganize

--- signup.py
+++ signup.py
@@ -1,68 +1,52 @@
 import webapp2
-import cgi
 import re
+from google.appengine.ext import db
+import random
+import hashlib
+import string
+import jinja2
+import os
+
+
+# loads jinja2
+jinja_environment = jinja2.Environment(autoescape = True,
+        loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    email = db.StringProperty(required = False)
+    salt = db.StringProperty(required = True)
+    hashed = db.StringProperty(required = True)
+
+
+def make_hashed_password(username, password, salt = None):
+    if not salt:
+        salt = ''.join(random.choice(string.letters) for x in xrange(5))
+    h = hashlib.sha256(username + password + salt).hexdigest()
+    return salt, h
 
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        username = cgi.escape(self.request.get('username'), quote = True)
-        self.response.out.write('Welcome, %s' % username)
-
-
-signup_form = """
-<form method="post">
-    <h2>
-        Sign up
-    </h2>
-    <div>
-        <label>
-            Username
-            <input type="text" name="username" value="%(username)s">
-        </label>
-        %(uerr)s
-    </div>
-    <br>
-
-    <div>
-        <label>
-            Password
-            <input type="password" name="password">
-        </label>
-        %(perr)s
-    </div>
-    <br>
-
-    <label>
-        Verify password
-        <input type="password" name="verify">
-    </label>
-    <br> <br>
-
-    <div>
-        <label>
-            Email (optional)
-            <input type="text" name="email" value="%(email)s">
-        </label>
-        %(eerr)s
-    </div>
-    <br> <br>
-    <input type="submit">
-</form>
-"""
+        uid = self.request.cookies.get('uid')
+        if not uid:
+            self.redirect('/signup')
+            return
+        user_id = int(uid)
+        user = User.get_by_id(user_id)
+        if not user:
+            self.redirect('/signup')
+            return
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Welcome, %s' % user.username)
 
 
 class SignupHandler(webapp2.RequestHandler):
-    def write_form(self, username = '', email = '', uerr = '', perr = '',
-                         eerr = ''):
+    def write_form(self, **kwargs):
         #curs = db.GqlQuery("SELECT * FROM User");
-
-        username = cgi.escape(username, quote = True)
-        email = cgi.escape(email, quote = True)
-        self.response.out.write(signup_form % {'username': username,
-                                                'email': email,
-                                                'uerr': uerr,
-                                                'perr': perr,
-                                                'eerr': eerr})
+        template = jinja_environment.get_template('signup.html')
+        self.response.out.write(template.render(kwargs))
 
     def get(self):
         self.write_form()
@@ -94,11 +78,14 @@ class SignupHandler(webapp2.RequestHandler):
 
         if ok:
             # create an User entity and insert into the DataStore
-            #user = User(username = username, password = password,
-                        #email = email)
-            #user.put()
-            self.redirect('/welcome?username=%s' % username)
+            salt, hashed_pwd = make_hashed_password(username, password)
+            user = User(username = username, email = email,
+                        salt = salt, hashed = hashed_pwd)
+            user.put()
+            self.response.headers.add_header('Set-Cookie', 'uid=%s' % (user.key().id()))
+            print "new user: %s, %s, %s, %s" % (username, email, salt, hashed_pwd)
+            self.redirect('/welcome')
         else:
-            self.write_form(username, email, uerr, perr, eerr)
+            self.write_form(username = username, email = email, uerr = uerr, perr = perr, eerr = eerr)
 
 
